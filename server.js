478e4bab7eeb60f58c96489793ee2a66efa7a536
brix-apg <briandacallos39@gmail.com>
2024-08-01 00:32:15 +0800

    adding new files

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
+import authRoute from './route/authRoute.js'
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoute)
 
 try {
     await mongoose.connect(process.env.MONGO_URL);
